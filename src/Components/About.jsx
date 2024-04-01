ca09319e6b0a696a05a515992d6f9c044a2846ec
Pradyot <sonipradyot@gmail.com>
2024-04-01 14:42:20 +0000

    hi there

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,9 +16,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/2.jpg";
+import image from "../images/3.jpg";
 
-const imageAltText = "Portfolio image 2";
+const imageAltText = "Portfolio image 3";
 
 /**
  * Sort description that expands on your title on the Home component.
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/3.jpg";
+import image from "../images/2.jpg";
 
-const imageAltText = "Potfolio image 3";
+const imageAltText = "Potfolio image 2";
 
 const Home = ({ name, title }) => {
   return (
